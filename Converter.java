ad5281fe506c4c293e70c64cc0b96f3c6c0c2ea7
Lucas-Luwa <lucasluwa@yahoo.com>
2022-07-24 21:47:47 -0400

    Fully Complete
    7.24.22 @ 9:47pm et

--- Converter.java
+++ Converter.java
@@ -12,9 +12,9 @@ public class Converter{
     }
     public static void mainConverter(File myCSV){
         File myOutput = new File("Output.txt");
-        String[] valueholder = {"<table>", "</table>","<td>","</td>","<tr>","</tr>","<th>","</th>"};
         try{
             FileWriter writer = new FileWriter(myOutput);
+            writer.write("<table>\n");
             Scanner fileScan = new Scanner(myCSV);
             int runNum = 0;
             String currLine = "";
@@ -24,18 +24,23 @@ public class Converter{
                 // System.out.println("here");
                 currLine = fileScan.nextLine();
                 items = currLine.split(",");
-                String newLine = valueholder[4];
-                for(int i = 0; i < items.length; i++){
-                    newLine = newLine + valueholder[6] + items[i]+ valueholder[7];
-                }
-                writer.write(newLine+"\n");
-
+                if(runNum==0)writer.write(coreSystems(items, 4, 6));
+                else writer.write(coreSystems(items,4,2));
                 runNum++;
             }
-            System.out.println(runNum);//Check Run Numbers
+            // System.out.println(runNum); //Check Run Numbers
+            writer.write("<\\table>\n");
             writer.close();
             fileScan.close();
         }catch(IOException e){
         }
+    }
+    public static String coreSystems(String[] items, int ctrl1, int ctrl2){
+        String[] valueholder = {"<table>", "</table>","<td>","</td>","<tr>","</tr>","<th>","</th>"};
+        String newLine = valueholder[ctrl1];
+        for(int i = 0; i < items.length; i++){
+            newLine = newLine + valueholder[ctrl2] + items[i]+ valueholder[ctrl2+1];
+        }
+        return newLine+valueholder[ctrl1+1]+"\n";
     }
 }
